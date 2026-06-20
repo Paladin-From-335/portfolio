0c01710fa029bcbc5105ec54194f852ea2f153f8
paladin-from-335 <andriia@andriis-macbook-pro6.home>
2026-06-20 23:43:38 +0200

    home page selected works + cleanup

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './components/hero/hero.component';
+import { SelectedSectionComponent } from './components/selected-section/selected-section.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent],
+  imports: [HeroComponent, SelectedSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
