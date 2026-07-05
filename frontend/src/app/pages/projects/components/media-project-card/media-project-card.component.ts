cbd985ba3587bc9310c2c0f509a87b6d0fb39ec2
paladin-from-335 <andriia@andriis-macbook-pro6.home>
2026-07-06 01:30:11 +0200

    cleanup + refactoring

---
+++ frontend/src/app/pages/projects/components/media-project-card/media-project-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { PhotoCarouselComponent } from '../../../../shared/components/photo-carousel/photo-carousel.component';
+import { ProjectCard } from '../../../../core/models/project-card.model';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { YoutubeEmbedService } from '../../../../core/services/YoutubeEmbedService';
+import {
+  YoutubeVideoPreviewComponent
+} from '../../../../shared/components/video-preview/youtube-video-preview.component';
+
+@Component({
+  selector: 'app-media-project-card',
+  imports: [PhotoCarouselComponent, YoutubeVideoPreviewComponent],
+  templateUrl: './media-project-card.component.html',
+  styleUrl: './media-project-card.component.scss',
+})
+export class MediaProjectCardComponent {
+  @Input({ required: true }) project!: ProjectCard;
+}
