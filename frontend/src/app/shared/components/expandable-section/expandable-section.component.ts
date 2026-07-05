ecffb42ef598788eecdf61d100bcd19fc059be99
paladin-from-335 <andriia@andriis-macbook-pro6.home>
2026-07-06 01:29:11 +0200

    expandable section - reusable for several purposes: whole sections of content, sub-sections like description, etc

---
+++ frontend/src/app/shared/components/expandable-section/expandable-section.component.ts
@@ -0,0 +1,17 @@
+import { Component, input, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-expandable-section',
+  standalone: true,
+  templateUrl: './expandable-section.component.html',
+  styleUrl: './expandable-section.component.scss',
+})
+export class ExpandableSectionComponent {
+  title = input<string>();
+  variant = input<'default' | 'description' > ('default');
+  isExpanded = signal(false);
+
+  toggleExpanded() {
+    this.isExpanded.update(v => !v);
+  }
+}
